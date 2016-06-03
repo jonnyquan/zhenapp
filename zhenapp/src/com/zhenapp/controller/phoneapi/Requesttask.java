12be14413e3220793794d2877c02815984997eb7
wuxiaofan <319809430@qq.com>
2016-06-03 17:37:00 +0800

    20160603173600

--- zhenapp/src/com/zhenapp/controller/phoneapi/Requesttask.java
+++ zhenapp/src/com/zhenapp/controller/phoneapi/Requesttask.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import com.zhenapp.service.TaskDetailInfoService;
 import com.zhenapp.service.TaskInfoService;
 import com.zhenapp.service.TbaccountInfoService;
 
+@Scope("prototype")
 @Controller
 public class Requesttask {
 	@Autowired
